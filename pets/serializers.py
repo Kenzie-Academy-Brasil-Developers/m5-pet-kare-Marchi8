87c670a071c39a1fe98ba70d47e97b7ff0c6da93
Marchi8 <luandeoliveirasoares@gmail.com>
2022-12-01 17:20:18 -0300

    feat: models diagram and pets crud

--- pets/serializers.py
+++ pets/serializers.py
@@ -16,13 +16,36 @@ class PetSerializer(serializers.Serializer):
         default=Sex.Not_Informed,
     )
     group = GroupSerializer()
+    traits_count = serializers.SerializerMethodField(
+        method_name="trait_count",
+        read_only=True,
+    )
     traits = TraitSerializer(many=True)
 
+    def trait_count(self, obj: Pet) -> int:
+        return len(obj.traits.all())
+
     def create(self, validated_data: dict) -> Pet:
-        pet = Pet.objects.create(**validated_data)
+        group_dict = validated_data.pop("group")
+        traits_list = validated_data.pop("traits")
+        group = Group.objects.get_or_create(**group_dict)[0]
+        pet = Pet.objects.create(**validated_data, group=group)
+        for trait in traits_list:
+            trait_obj = Trait.objects.get_or_create(**trait)[0]
+            pet.traits.add(trait_obj)
         return pet
 
-    def update(self, instance, validated_data: dict):
+    def update(self, instance: Pet, validated_data: dict) -> Pet:
+        if "group" in validated_data:
+            group_dict = validated_data.pop("group")
+            group_obj = Group.objects.get_or_create(**group_dict)[0]
+            instance.group = group_obj
+        if "traits" in validated_data:
+            traits_list = validated_data.pop("traits")
+            for trait in traits_list:
+                trait_obj = Trait.objects.get_or_create(**trait)[0]
+                instance.traits.clear()
+                instance.traits.add(trait_obj)
         for key, value in validated_data.items():
             setattr(instance, key, value)
         instance.save()
--- pets/urls.py
+++ pets/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-
 from pets.views import PetView, PetViewTest
 
 urlpatterns = [
